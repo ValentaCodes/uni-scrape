5ceca9fbb3f378c407ac1247fafdeb092b9e1264
ValentaCodes <valentacodes@gmail.com>
2026-07-06 22:48:31 -0600

    housekeeping

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 # Week 2 checkpoint - Fetch data from real webpage and pull data out of it
 # Week 3 checkpoint - Clean data and store it in database
 
-import csv
-from scrape import scrape
 from transform import transform
 from load import load
+from driver import driver
 """
 Enter uni name
 scaper should gather program information 
@@ -14,9 +13,11 @@ categorize and data and save as a CSV file
 """
 
 def main():
-    books = scrape()
-    data_frame = transform(books)
-    load(data_frame)
+    url = "https://books.toscrape.com/"
+
+    books = driver(url)
+    transform(books)
+    # load(data_frame)
 
 
 if __name__ == "__main__":
