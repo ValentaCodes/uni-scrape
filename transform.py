0382d229ca803943dee64715c34e4ae0cf22af3e
ValentaCodes <valentacodes@gmail.com>
2026-07-06 22:48:54 -0600

    removed sorting

--- transform.py
+++ transform.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 def transform(books):
-    df = pd.DataFrame(books, columns=["Title", "Price", "Scraped_Time"])
-    df.sort_values(by=["Price"], ascending=True, inplace=True)
+    df = pd.DataFrame(books)
     df.to_csv("books.csv", index=False)
-
     return df
 
