64933ebb021e11d7635328a3f9ff89641cc28702
ValentaCodes <valentacodes@gmail.com>
2026-07-08 12:26:14 -0600

    added docstring and csv file creation

--- transform.py
+++ transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def transform(books):
+    """Transforms books into dataframe and creates a CSV file"""
     df = pd.DataFrame(books)
-    df.to_csv("books.csv", index=False)
+    df.to_csv('books.csv')
     return df
